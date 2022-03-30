4ea6bfa1f0dfdf95d52f4fe596398eedaecfcb1b
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2022-03-30 10:23:28 +0700

    commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data(){
+        return {
+            product:'Socks',
+            description: 'This is a beautiful socks.',
+            image: './assets/images/socks_green.jpg',
+        }
+    }
+})
